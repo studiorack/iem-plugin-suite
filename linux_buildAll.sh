5930eb04fb6d67f639a3505515d887fd2b6d5d19
Sebastian Grill <sebastian.grill@gmx.at>
2017-12-18 22:53:41 +0100

    added linux build_all script

---
+++ linux_buildAll.sh
@@ -0,0 +1,13 @@
+for d in */; do
+    echo "Compiling $d for Linux..."
+    if [ -d "$PWD/${d}Builds/LinuxMakefile" ]; then
+        cd "$PWD/${d}Builds/LinuxMakefile"
+        make CONFIG=Release AR=gcc-ar -j 6
+        cd ../../../
+        echo "done..."
+    fi
+    if [ ! -d "$PWD/${d}Builds/LinuxMakefile" ]; then
+        echo "no makefile found, moving on..."
+    fi
+done
+echo "all done!"
