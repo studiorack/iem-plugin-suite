aac8124aa4fd46c1f6b1bb90012c2e92585be7ed
IOhannes m zmölnig <zmoelnig@iem.at>
2017-12-19 13:04:38 +0100

    iterate over .jucer files rather than over directories

--- macOS_buildAll.sh
+++ macOS_buildAll.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 cd ${0%/*}
 mkdir -p _compiledPlugins/macOS/IEM
-for d in */; do
+for d in */*.jucer; do
+  d=${d%/*}
   echo "Compiling $d for macOS..."
-    if [ -d "$PWD/${d}Builds/MacOSX" ]; then
-        cd "$PWD/${d}Builds/MacOSX"
-        xcodebuild -target "${d%/} - All" -configuration "Release" build
+    if [ -d "${d}/Builds/MacOSX" ]; then
+        cd "${d}/Builds/MacOSX"
+        xcodebuild -target "${d} - All" -configuration "Release" build
         echo "done..."
         cd "../../../"
-        cp -R -H "${d}/Builds/MacOSX/build/Release/${d%/}.vst" "_compiledPlugins/macOS/IEM/"
+        cp -R -H "${d}/Builds/MacOSX/build/Release/${d}.vst" "_compiledPlugins/macOS/IEM/"
     else
         echo "no xcode project found, moving on..."
     fi
