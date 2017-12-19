635493972900748ec48b67beb6cbb61a81f2d7e4
IOhannes m zmölnig <zmoelnig@iem.at>
2017-12-19 12:56:07 +0100

    dropped ${PWD} from path (we are there anyhow)

--- linux_buildAll.sh
+++ linux_buildAll.sh
@@ -2,8 +2,8 @@
 cd ${0#/*}
 for d in */; do
     echo "Compiling $d for Linux..."
-    if [ -d "$PWD/${d}Builds/LinuxMakefile" ]; then
-        make CONFIG=Release AR=gcc-ar -j$(nproc) -k -C "$PWD/${d}Builds/LinuxMakefile"
+    if [ -d "${d}Builds/LinuxMakefile" ]; then
+        make CONFIG=Release AR=gcc-ar -j$(nproc) -k -C "${d}Builds/LinuxMakefile"
         echo "done..."
     else
         echo "no makefile found, moving on..."
