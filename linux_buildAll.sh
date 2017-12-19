a3662f6e1563936237848b37d5f3bd852e04336d
IOhannes m zmölnig <zmoelnig@iem.at>
2017-12-19 12:52:54 +0100

    keep on building, even if it fails...

--- linux_buildAll.sh
+++ linux_buildAll.sh
@@ -3,7 +3,7 @@ cd ${0#/*}
 for d in */; do
     echo "Compiling $d for Linux..."
     if [ -d "$PWD/${d}Builds/LinuxMakefile" ]; then
-        make CONFIG=Release AR=gcc-ar -j$(nproc) -C "$PWD/${d}Builds/LinuxMakefile"
+        make CONFIG=Release AR=gcc-ar -j$(nproc) -k -C "$PWD/${d}Builds/LinuxMakefile"
         echo "done..."
     fi
     if [ ! -d "$PWD/${d}Builds/LinuxMakefile" ]; then
