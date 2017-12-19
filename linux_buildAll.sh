f02d5339c502cb5d0ff07d81f7ad5e2bd166c4bd
IOhannes m zmölnig <zmoelnig@iem.at>
2017-12-19 12:59:01 +0100

    iterate over .jucer files, rather than directories

--- linux_buildAll.sh
+++ linux_buildAll.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 cd ${0%/*}
-for d in */; do
+for d in */*.jucer; do
+    d=${d%/*}
     echo "Compiling $d for Linux..."
-    if [ -d "${d}Builds/LinuxMakefile" ]; then
-        make CONFIG=Release AR=gcc-ar -j$(nproc) -k -C "${d}Builds/LinuxMakefile"
+    if [ -d "${d}/Builds/LinuxMakefile" ]; then
+        make CONFIG=Release AR=gcc-ar -j$(nproc) -k -C "${d}/Builds/LinuxMakefile"
         echo "done..."
     else
         echo "no makefile found, moving on..."
