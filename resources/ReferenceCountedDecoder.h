8d47cfdbd716a42602a210d16b8fb354fe654e5a
Daniel Rudrich <mail@danielrudrich.de>
2018-01-06 23:24:24 +0100

    new class for decodes (work in progress)

---
+++ resources/ReferenceCountedDecoder.h
@@ -0,0 +1,74 @@
+/*
+ ==============================================================================
+ This file is part of the IEM plug-in suite.
+ Author: Daniel Rudrich
+ Copyright (c) 2017 - Institute of Electronic Music and Acoustics (IEM)
+ https://www.iem.at
+ 
+ The IEM plug-in suite is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ The IEM plug-in suite is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this software.  If not, see <https://www.gnu.org/licenses/>.
+ ==============================================================================
+ */
+
+#pragma once
+#include "Eigen/Dense"
+
+class ReferenceCountedDecoder : public ReferenceCountedObject
+{
+    enum Normalization
+    {
+        n3d,
+        sn3d
+    };
+    
+    struct Settings {
+        Normalization expectedNormalization = sn3d;
+    };
+    
+public:
+    typedef ReferenceCountedObjectPtr<ReferenceCountedDecoder> Ptr;
+    
+    ReferenceCountedDecoder (const String& nameToUse, const String& descriptionToUse, int rows, int columns)
+    :   name (nameToUse), description (descriptionToUse), matrix (rows, columns)
+    {
+        DBG ("Decoder named '" << name << "' constructed. Size: " << rows << "x" << columns);
+    }
+    
+    ~ReferenceCountedDecoder()
+    {
+        DBG (String ("Decoder named '") + name + "' destroyed.");
+    }
+    
+    Eigen::MatrixXf* getMatrix()
+    {
+        return &matrix;
+    }
+    const String getName()
+    {
+        return name;
+    }
+    
+    const String getDescription()
+    {
+        return description;
+    }
+    
+    
+private:
+    String name;
+    String description;
+    Eigen::MatrixXf matrix;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ReferenceCountedDecoder)
+};
+
