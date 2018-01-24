c7f784686c860874d9c825f7cf34685786d21443
Daniel Rudrich <mail@danielrudrich.de>
2018-01-24 15:10:07 +0100

    added more floor reflections to RoomEncoder

--- RoomEncoder/createListOfReflections.m
+++ RoomEncoder/createListOfReflections.m
@@ -1,6 +1,6 @@
 xRange = -3:3;
 yRange = -3:3;
-zRange = -0:2;
+zRange = -2:2;
 
 cmbs = allcomb(xRange,yRange,zRange);
 order = sum(abs(cmbs),2);
@@ -9,5 +9,5 @@ cmbs = cmbs(idx,:);
 
 sum(order <= 7)
 refl = [cmbs(order<=7,:) order(order<=7)];
-
+%%
 mat2header('reflections',refl,1);
