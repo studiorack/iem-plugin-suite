7c85b07aca74b53de1068c577de72f26be6e5722
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2018-05-14 21:16:12 +0200

    [ci] dropped extraneous creation of OUTDIT

--- .git-ci/getjuce.sh
+++ .git-ci/getjuce.sh
@@ -6,6 +6,5 @@ OUTDIR=$2
 mkdir -p "${OUTDIR}"
 
 curl -o juce.zip https://d30pueezughrda.cloudfront.net/juce/juce-${JUCEFLAVOUR}.zip
-mkdir -p "${OUTDIR}"
 unzip -q juce.zip -d "${OUTDIR}"
 mv "${OUTDIR}"/JUCE/* "${OUTDIR}" || true
