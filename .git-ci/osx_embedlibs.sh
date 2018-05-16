d9e71915012f671f4971ec9fac43177f6388ab01
IOhannes m zmölnig <zmoelnig@iem.at>
2018-05-16 15:09:48 +0200

    [ci] script to embed non-standard libraries in OSX bundles

---
+++ .git-ci/osx_embedlibs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+embed() {
+  bin=$1
+  otool -L "$bin" | awk '{print $1}' | grep "^/usr/local" | while read lib; do
+    mkdir -p ../Frameworks/
+    cp -r "${lib}" ../Frameworks/
+    embedded=@loader_path/../Frameworks/${lib##*/}
+    install_name_tool -change "${lib}" "${embedded}" "${bin}"
+  done
+}
+
+for f in "$@"; do
+  (cd "${f%/*}"; embed "${f##*/}")
+done
+
