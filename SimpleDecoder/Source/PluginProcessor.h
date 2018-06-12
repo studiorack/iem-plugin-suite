4bfabae92265d051697680788f2dec6be04a7848
IOhannes m zmölnig <zmoelnig@iem.at>
2018-06-12 16:17:27 +0200

    renamed resources/decoderHelper.h to resources/DecoderHelper.h
    @rudrich there are filesystems that are case-sensitive...

--- SimpleDecoder/Source/PluginProcessor.h
+++ SimpleDecoder/Source/PluginProcessor.h
@@ -25,7 +25,7 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "../../resources/IOHelper.h"
 #include "../../resources/AmbisonicDecoder.h"
-#include "../../resources/decoderHelper.h"
+#include "../../resources/DecoderHelper.h"
 #include "../../resources/ReferenceCountedDecoder.h"
 #include "../../resources/FilterVisualizerHelper.h"
 
