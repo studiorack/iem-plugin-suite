deae6ae4933e10f2b2439f6bb1b50f070a9b2a2d
Daniel Rudrich <mail@danielrudrich.de>
2018-11-11 12:20:13 +0100

    making fix compatible with older states

--- AllRADecoder/Source/PluginProcessor.cpp
+++ AllRADecoder/Source/PluginProcessor.cpp
@@ -368,7 +368,7 @@ void AllRADecoderAudioProcessor::setStateInformation (const void* data, int size
             for (int i = 0; i < nChilds; ++i)
             {
                 XmlElement* lsp (lsps->getChildElement(i));
-                if (lsp->getTagName() == "Element")
+                if (lsp->getTagName() == "Element" || lsp->getTagName() == "Loudspeaker")
                     loudspeakers.appendChild(createLoudspeakerFromSpherical(Vector3D<float> (lsp->getDoubleAttribute("Radius", 1.0),
                                                                                              lsp->getDoubleAttribute("Azimuth"),
                                                                                              lsp->getDoubleAttribute("Elevation")),
