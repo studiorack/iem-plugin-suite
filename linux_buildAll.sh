3f308fc0527e98889569d4ebba45b961e3a53d94
Daniel Rudrich <mail@danielrudrich.de>
2019-01-30 12:54:03 +0100

    trying to create makefiles automatically

--- linux_buildAll.sh
+++ linux_buildAll.sh
@@ -2,8 +2,22 @@
 cd ${0%/*}
 mkdir -p _compiledPlugins/linux/IEM
 mkdir -p _compiledPlugins/linux/Standalone
+
+# search for projucer (should be configured with global pathes before)
+projucer=$(pwd)/JUCE/Projucer
+if [ ! -x ${projucer} ]; then
+    projucer=$(which Projucer)
+    if [ ! -x ${projucer} ]; then
+        echo no $projucer found, maybe copy it there
+    fi
+fi
+echo using $projucer as Projucer
+
 for d in */*.jucer; do
     d=${d%/*}
+
+    if [ -x "${projucer}" ]; then ${projucer} --resave ${f}; fi  
+
     echo "Compiling $d for Linux..."
     if [ -d "${d}/Builds/LinuxMakefile" ]; then
         make CONFIG=Release AR=gcc-ar -j$(nproc) -k -C "${d}/Builds/LinuxMakefile"
